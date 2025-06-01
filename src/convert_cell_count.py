a767e03b3cf7dad8245aae2eb07017bb8a9bd036
huytran2511 <tranquanghuy999@gmail.com>
2025-06-01 02:42:07 -0700

    calculate total_count for each sample

--- src/convert_cell_count.py
+++ src/convert_cell_count.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('cell-count.csv')
+input_file = 'cell-count.csv'
+df = pd.read_csv(input_file)
+
+cell_types = ['b_cell', 'cd8_t_cell', 'cd4_t_cell', 'nk_cell', 'monocyte']
+
+df['total_count'] = df[cell_types].sum(axis=1)
 
 print(df.head())
